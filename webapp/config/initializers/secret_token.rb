754f2b66d8acda38bee5b9a2e7ba10d649a4f3b7
chottoda <Yasutoshi.Hiroe@jrits.ricoh.co.jp>
2015-12-18 10:51:04 +0900

    bundle exec rails new webapp --skip-bundle

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '436762383b2f316230d3c5f2bc596c0002a06d022a7661575fc7077189cd95868bd00efb37bc014489962d7141c04f89d1bcb7c4f43e0839a7fbc88252df401f'
